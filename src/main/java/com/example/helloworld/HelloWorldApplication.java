ff6acf69b1722afee89b2c62e1580cf61cd087fd
Tanvi Mehta <tanvimehta@Tanvis-MacBook-Pro.local>
2015-01-12 20:33:55 -0500

    Server Setup. Hello World.

---
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,41 @@
+package com.example.helloworld;
+
+import com.example.helloworld.core.Template;
+import com.example.helloworld.health.TemplateHealthCheck;
+import com.example.helloworld.resources.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import io.dropwizard.views.ViewBundle;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        bootstrap.addBundle(new MigrationsBundle<HelloWorldConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(HelloWorldConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        });
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+        final Template template = configuration.buildTemplate();
+
+        environment.healthChecks().register("template", new TemplateHealthCheck(template));
+        environment.jersey().register(new HelloWorldResource(template));
+    }
+}
