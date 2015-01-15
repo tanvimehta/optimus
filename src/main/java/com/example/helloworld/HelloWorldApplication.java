790b3cd9259ac88a664a9fa17100dc427e24b2c3
Tanvi Mehta <tanvimehta@user3-223-222.wireless.utoronto.ca>
2015-01-15 16:11:42 -0500

    Dummy comment

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -11,6 +11,7 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
 
+// Hello World App
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
     public static void main(String[] args) throws Exception {
         new HelloWorldApplication().run(args);
