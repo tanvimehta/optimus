0f6804877b3084a59f87312396010e75983cdcdf
Tanvi Mehta <tanvimehta@Tanvis-MacBook-Pro.local>
2015-01-20 21:08:38 -0500

    Routing integrated - neo4j

--- src/main/java/com/uplan/neo4j/ResourceNode.java
+++ src/main/java/com/uplan/neo4j/ResourceNode.java
@@ -1,17 +1,84 @@
 package com.uplan.neo4j;
 
+import org.neo4j.graphdb.Direction;
+import org.springframework.data.neo4j.annotation.Fetch;
 import org.springframework.data.neo4j.annotation.GraphId;
+import org.springframework.data.neo4j.annotation.NodeEntity;
+import org.springframework.data.neo4j.annotation.RelatedTo;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Created by tanvimehta on 15-01-18.
- */
-public class LocationNode {
+* Created by tanvimehta on 15-01-18.
+*/
+@NodeEntity
+public class ResourceNode {
 
-    @GraphId Long id;
-    public String resourceType;
+    @GraphId
+    Long id;
+    private Long resourceId;
+    private String resourceType;
+    private String resourceName;
 
+    public ResourceNode() {}
 
-    public LocationNode() {}
-    public LocationNode()
+    public ResourceNode(long resourceId, String resourceType, String resourceName) {
+        this.resourceId = resourceId;
+        this.resourceType = resourceType;
+        this.resourceName = resourceName;
+    }
 
+    @RelatedTo(type="CONNECTS_TO", direction = Direction.BOTH)
+    public @Fetch
+    Set<ResourceNode> neighbours;
+
+    public void connectsTo(ResourceNode resourceNode) {
+        if (neighbours == null) {
+            neighbours = new HashSet<ResourceNode>();
+        }
+        neighbours.add(resourceNode);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public Long getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(Long resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public String toString() {
+        String results = resourceName + " has the type " + resourceType +" and its neighbours include\n";
+        if (neighbours != null) {
+            for (ResourceNode resourceNode : neighbours) {
+                results += "\t- " + resourceNode.resourceName + "\n";
+            }
+        }
+        return results;
+    }
 }
--- src/main/java/com/uplan/neo4j/ResourceRepository.java
+++ src/main/java/com/uplan/neo4j/ResourceRepository.java
@@ -1,7 +1,20 @@
 package com.uplan.neo4j;
 
+import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.neo4j.core.EntityPath;
+import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.neo4j.repository.RelationshipOperationsRepository;
+import org.springframework.data.repository.CrudRepository;
+
 /**
- * Created by tanvimehta on 15-01-18.
- */
-public class ResourceRepository {
+* Created by tanvimehta on 15-01-18.
+*/
+public interface ResourceRepository extends CrudRepository<ResourceNode, Long>, RelationshipOperationsRepository<ResourceNode>, GraphRepository<ResourceNode> {
+
+    ResourceNode findByResourceId(Long resourceId);
+
+    Iterable<ResourceNode> findByNeighboursResourceId(Long resourceId);
+
+    @Query("START r1=node({0}), r2=node({1}) MATCH p = shortestPath( r1-[*]-r2 ) RETURN p")
+    Iterable<EntityPath<ResourceNode, ResourceNode>> getPath(ResourceNode r1, ResourceNode r2);
 }
